7691560597e2042c05b115a8b3e857c638fa61a1
Aslan Mammadov <mammadovaslan21@gmail.com>
2023-05-20 01:04:49 +0400

    collecting highlighters data in object on line 20

--- src/js/marker.js
+++ src/js/marker.js
@@ -1,5 +1,5 @@
 // Set initial color value
-let color = "";
+let color = "rgba(255, 238, 0, 0.5)";
 const transparency = 0.5; //set highligh transparancy
 
 // Handle color change event
@@ -9,20 +9,23 @@ colorInput.addEventListener("change", (e) => {
 });
 
 // Convert Hex to RGBA
-function hexToRgba(hex, alpha) {
+const hexToRgba = (hex, alpha) => {
   const r = parseInt(hex.slice(1, 3), 16);
   const g = parseInt(hex.slice(3, 5), 16);
   const b = parseInt(hex.slice(5, 7), 16);
   return `rgba(${r}, ${g}, ${b}, ${alpha})`;
-}
+};
 
 let markCounter = 1;
+const data = []; //!This data should be sent to the server
 
 // Highlight selected text
-function highlightSelectedText() {
+const highlightSelectedText = (e) => {
   const markInstance = new Mark(document.body);
 
   const range = getSelectionStartAndLength(document.body);
+  const start = range.start; // || data.range.start
+  const length = range.length; // || data.range.length
 
   markInstance.markRanges([range], {
     acrossElements: false,
@@ -34,10 +37,13 @@ function highlightSelectedText() {
     },
   });
 
-  markCounter++;
-}
+  if (!!start && !!length && !e.target.classList.contains("color-input"))
+    data.push({ start, length, color, counter: markCounter }); //sending data object
 
-function getSelectionStartAndLength(element) {
+  markCounter++;
+};
+
+const getSelectionStartAndLength = (element) => {
   let start = 0;
   let length = 0;
   let sel, range, priorRange;
@@ -67,6 +73,6 @@ function getSelectionStartAndLength(element) {
     start,
     length,
   };
-}
+};
 
 document.addEventListener("mouseup", highlightSelectedText);
